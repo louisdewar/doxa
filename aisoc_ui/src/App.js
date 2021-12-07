1ad32e1ca28b7c31a34639a7502a970c1206b278
Jeremy <jeremyloyingping@googlemail.com>
2021-12-07 23:14:30 +0000

    Lazy-load competitions in the UI.

--- aisoc_ui/src/App.js
+++ aisoc_ui/src/App.js
@@ -1,8 +1,5 @@
-// import Landing from 'pages/Landing.js';
-import ClimateHack from 'competitions/climatehack/ClimateHack';
-import Uttt from 'competitions/uttt/Uttt';
 import { AuthProvider, useAuth } from 'hooks/useAuth';
-import { Suspense } from 'react';
+import { lazy, Suspense } from 'react';
 import {
   BrowserRouter as Router, Redirect, Route, Switch
 } from 'react-router-dom';
@@ -10,8 +7,8 @@ import './App.scss';
 
 
 const COMPETITIONS = {
-  climatehack: ClimateHack,
-  uttt: Uttt,
+  climatehack: lazy(() => import('competitions/climatehack/ClimateHack')),
+  uttt: lazy(() => import('competitions/uttt/Uttt')),
 };
 
 const DEFAULT_COMPETITION = process.env.REACT_APP_DEFAULT_COMPETITION ?? 'uttt';
@@ -23,7 +20,7 @@ const DEFAULT_COMPETITION = process.env.REACT_APP_DEFAULT_COMPETITION ?? 'uttt';
  * is in the process of being determined.
  */
 function Loading() {
-  return <span>Loading...</span>;
+  return <span></span>;
 }
 
 
