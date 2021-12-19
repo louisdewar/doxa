611316bcedbe164de59b153a68c7b2ae98719348
Louis de Wardt <contact@louis.dewardt.uk>
2021-12-19 21:48:26 +0000

    progressive & parallel loading for user page (uttt)

--- aisoc/client/src/competitions/uttt/pages/User.js
+++ aisoc/client/src/competitions/uttt/pages/User.js
@@ -1,37 +1,95 @@
 import Card from 'components/Card';
+import LoadingPlaceholder from 'components/LoadingPlaceholder';
 import PairMatches from 'components/PairMatches';
 import { useEffect, useState } from 'react';
 import { useParams } from 'react-router-dom';
 import UTTTAPI from '../api';
+import { Link } from 'react-router-dom';
+
+async function fetchMatchRow(setPlayers, setScores, matchID) {
+  setPlayers(null);
+  setScores(null);
+
+  const players = await UTTTAPI.getGamePlayers(matchID);
+
+  setPlayers(players.map(player => player.username));
+
+  const score1 = await UTTTAPI.getGameResult(matchID, players[0].agent);
+  const score2 = await UTTTAPI.getGameResult(matchID, players[0].agent);
+
+  setScores([score1, score2]);
+}
+
+function MatchRow({ filter, matchID, baseUrl, i }) {
+  const [players, setPlayers] = useState(null);
+  const [scores, setScores] = useState(null);
+
+  useEffect(async () => {
+    await fetchMatchRow(setPlayers, setScores, matchID);
+  }, [matchID]);
+
+  let username;
+  if (players) {
+    const [player1, player2] = players;
+
+    if (!player1.includes(filter) && !player2.includes(filter)) {
+      return null;
+    }
+
+    if (scores) {
+      const [score1, score2] = scores;
+
+      username = (
+        <>
+          <Link to={`${baseUrl}user/${player1}`}>{player1}</Link> {' '}
+          ({score1})
+          vs {' '}
+          <Link to={`${baseUrl}user/${player2}`}>{player2}</Link> {' '}
+          ({score2})
+        </>
+      );
+    } else {
+      username = (
+        <>
+          <Link to={`${baseUrl}user/${player1}`}>{player1}</Link> {' '}
+          (<LoadingPlaceholder height={13} width={20} />)
+          vs {' '}
+          <Link to={`${baseUrl}user/${player2}`}>{player2}</Link> {' '}
+          (<LoadingPlaceholder height={13} width={20} />)
+        </>
+      );
+    }
+  } else {
+    username = (
+      <>
+        <LoadingPlaceholder height={15} width={'100%'} />
+      </>
+    );
+  }
+
+
+  return (
+    <div className='leaderboard-entry'>
+      <span className="leaderboard-position">{i + 1}</span>
+      <span className="leaderboard-username">{username}</span>
+      <span className="leaderboard-match-link"><Link to={`${baseUrl}match/${matchID}`}>View match</Link></span>
+    </div>
+  );
+}
 
 export default function User({ baseUrl }) {
   const { user } = useParams();
   const [score, setScore] = useState(0);
-  const [matches, setMatches] = useState([]);
+  const [matchIDs, setMatchIDs] = useState(null);
 
   useEffect(async () => {
     const data = await UTTTAPI.getUserScore(user);
     setScore(data.score || 0);
-    setMatches([]);
   }, [user]);
 
   useEffect(async () => {
     const activeGames = await UTTTAPI.getUserActiveGames(user);
-    const matches = [];
-
-    for (const activeGame of activeGames) {
-      const players = await UTTTAPI.getGamePlayers(activeGame.id);
-
-      matches.push({
-        id: activeGame.id,
-        player1: players[0].username,
-        player2: players[1].username,
-        score1: await UTTTAPI.getGameResult(activeGame.id, players[0].agent),
-        score2: await UTTTAPI.getGameResult(activeGame.id, players[1].agent)
-      });
-    }
-
-    setMatches(matches);
+    setMatchIDs(activeGames.map(game => game.id));
   }, [user]);
 
   return <>
@@ -41,9 +99,9 @@ export default function User({ baseUrl }) {
         {score} points
       </h2>
     </Card>
-    {matches && <Card>
+    {matchIDs && <Card>
       <h2>Matches</h2>
-      <PairMatches baseUrl={baseUrl} matches={matches} />
+      <PairMatches baseUrl={baseUrl} matchIDs={matchIDs} matchComponent={MatchRow} />
     </Card>}
   </>;
 }
--- aisoc/client/src/components/PairMatches.js
+++ aisoc/client/src/components/PairMatches.js
@@ -1,10 +1,8 @@
-import { useState } from 'react';
-import { Link } from 'react-router-dom';
+import { useState, Fragment } from 'react';
 import './Leaderboard.scss';
 import TextBox from './TextBox';
 
-
-export default function PairMatches({ baseUrl, matches }) {
+export default function PairMatches({ baseUrl, matchIDs, matchComponent }) {
   const [filter, setFilter] = useState('');
 
   return <div className="leaderboard">
@@ -21,12 +19,7 @@ export default function PairMatches({ baseUrl, matches }) {
       <span className="leaderboard-match-link">Match</span>
     </div>
 
-    {matches.map((entry, i) => (entry.player1.includes(filter) || entry.player2.includes(filter)) && <div key={i} className='leaderboard-entry'>
-      <span className="leaderboard-position">{i + 1}</span>
-      <span className="leaderboard-username">
-        <Link to={`${baseUrl}user/${entry.player1}`}>{entry.player1}</Link> {entry.score1 && `(${entry.score1})`} vs <Link to={`${baseUrl}user/${entry.player2}`}>{entry.player2}</Link> {entry.score2 && `(${entry.score2})`}
-      </span>
-      <span className="leaderboard-match-link"><Link to={`${baseUrl}match/${entry.id}`}>View match</Link></span>
-    </div>)}
+
+    {matchIDs.map((matchID, i) => <Fragment key={i}>{matchComponent({ matchID, filter, baseUrl, i })}</Fragment>)}
   </div>;
 }
