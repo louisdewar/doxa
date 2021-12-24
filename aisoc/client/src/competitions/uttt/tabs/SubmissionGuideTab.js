2b78ca30b2d458002fe095d86809d82cef9e1022
Jeremy <jeremyloyingping@googlemail.com>
2021-12-24 03:14:09 +0000

    Update the UTTT submission guide.

--- aisoc/client/src/competitions/uttt/tabs/SubmissionGuideTab.js
+++ aisoc/client/src/competitions/uttt/tabs/SubmissionGuideTab.js
@@ -7,32 +7,32 @@ export default function SubmissionGuideTab() {
       Ensure you have Python 3.7+ installed on your computer before you begin.
     </p>
     <p>
-      To get started, download the example agent project from <a href="https://github.com/louisdewar/doxa/releases/download/0.0.1/uttt_getting_started.zip">GitHub</a>.
+      To get started, download and extract the example agent project from <a href="https://github.com/louisdewar/doxa/releases/download/0.0.1/uttt_getting_started.zip">GitHub</a>.
     </p>
     <h3>Using the Doxa CLI</h3>
     <p>
-      To use the Doxa CLI, run the following command:
+      To use the Doxa CLI, run the following command from the extracted folder:
     </p>
     <pre>
       python doxa_cli.py --help
     </pre>
     <p>
-      If you do not have the relevant Doxa CLI binary for your operating system, it will be downloaded into the {'"bin"'} folder.
+      If you do not have the relevant Doxa CLI binary for your operating system, it will be downloaded into the <code>{'"bin"'}</code> folder.
     </p>
     <p>
-      Before you can submit your agent code, you must first login using the following command:
+      Before you can submit your agent code, you must first log in using the following command:
     </p>
     <pre>
       python doxa_cli.py user login
     </pre>
     <p>
-      When you want to submit, run the following command from the root of your agent folder:
+      When you want to upload your agent, run the following command:
     </p>
     <pre>
       python doxa_cli.py agent upload uttt ./agent
     </pre>
     <p>
-      Here, {'"./agent"'} refers to the folder containing the agent code you wish to submit.
+      Here, <code>{'"./agent"'}</code> refers to the folder containing the agent code you wish to submit.
     </p>
   </div>;
 }
