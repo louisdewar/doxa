93ff1f27edf0b67962e7a8b1825b4cfc709d2652
Jeremy <jeremyloyingping@googlemail.com>
2021-12-26 19:18:35 +0000

    Update OngoingCard.js

--- aisoc/client/src/competitions/uttt/pages/Match/OngoingCard.js
+++ aisoc/client/src/competitions/uttt/pages/Match/OngoingCard.js
@@ -9,7 +9,7 @@ export default function OngoingCard({ started }) {
         <div className="ongoing-icon"><FontAwesomeIcon icon={faClock} fixedWidth /></div>
         <div className="ongoing-message">
           <p>
-            This match {started ? 'is ongoing' : 'has been queued'}, so more games may appear here in the future.
+            This match {started ? 'is ongoing' : 'has been queued'}, so more games may appear here soon.
           </p>
         </div>
       </div>
