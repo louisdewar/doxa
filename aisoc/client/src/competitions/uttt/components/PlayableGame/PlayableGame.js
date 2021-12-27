7483d573d61b740f909939bef788c284782ea46a
Jeremy <jeremyloyingping@googlemail.com>
2021-12-27 00:12:46 +0000

    Lock the grid while the opponent is making a move in the playable game.

--- aisoc/client/src/competitions/uttt/components/PlayableGame/PlayableGame.js
+++ aisoc/client/src/competitions/uttt/components/PlayableGame/PlayableGame.js
@@ -5,6 +5,7 @@ import PlayableGameController from './PlayableGameController';
 
 export default function PlayableGame({ resets, agent }) {
   const controller = useRef(new PlayableGameController(agent));
+  const [locked, setLocked] = useState(false);
   const [grid, setGrid] = useState(() => {
     return controller.current.getGrid();
   });
@@ -20,9 +21,13 @@ export default function PlayableGame({ resets, agent }) {
   return <Grid
     gameState={grid}
     onTileClick={(g, t) => {
+      if (locked) return;
+
+      setLocked(true);
       for (const updatedGrid of controller.current.placeTile(g, t)) {
         setGrid(updatedGrid);
       }
+      setLocked(false);
     }}
   />;
 }
