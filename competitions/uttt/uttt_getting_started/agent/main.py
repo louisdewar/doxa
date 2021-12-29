8c7c70ddc5246180ac26e95807a4a4bc264f82f3
Jeremy <jeremyloyingping@googlemail.com>
2021-12-29 23:11:50 +0000

    Make the UTTT getting started code object oriented.
    I think I might've gone a bit overkill on the documentation, but I figured it might be helpful for competition participants new to Python.

--- competitions/uttt/uttt_getting_started/agent/main.py
+++ competitions/uttt/uttt_getting_started/agent/main.py
@@ -1,51 +1,68 @@
-# An example agent for UTTT competition.
-# It's designed to keep playing valid moves without any thought to strategy beyond that
-
 import random
+from typing import List, Optional, Tuple
 
-# Runs at startup to determine which player the current user is
-def get_player():
-    first_input = input()
-    if first_input == "S R":
-        return "R"
-    elif first_input == "S B":
-        return "B"
-    else:
-        raise Exception("The first message should be `S R` or `S B` got `{}`".format(first_input))
+from uttt import BaseAgent, UTTTGame
+
+
+class Agent(BaseAgent):
+    """
+    This is an example agent for the ultimate tic-tac-toe competition on Doxa.
+
+    Its current strategy is to play valid moves picked completely at random.
+
+    Feel free to modify this class to implement your own agent! You are welcome
+    to define additional methods as you see fit.
+    """
+
+    def make_move(
+        self,  # If you have never seen this syntax before, these are type annotations!
+        boards: List[List[Optional[str]]],  # Don't worry - they're totally optional.
+        board_winners: List[Optional[str]],
+        playable_boards: List[int],
+    ) -> Tuple[int, int]:
+        """Makes a move.
+
+        Args:
+            boards (List[List[Optional[str]]]): A list of local boards, which together form the global board.
+                                                Each local board is a list of nine tiles (indexed 0 to 8),
+                                                represented as either 'R' if marked by the red player,
+                                                'B' if marked by the blue player, or None if the tile is empty.
+
+            board_winners (List[Optional[str]]): The winners of each local board. While this totally random
+                                                 agent does not take local board winners into account, you will
+                                                 probably want to in order to implement a better strategy!
+
+            playable_boards (List[int]): The local boards that may be played in.
+
+        Returns:
+            Tuple[int, int]: The local board and tile position to mark for your agent.
+        """
+
+        # You can get the player you are currently playing as (R or B) with `self.player`.
+        # Likewise, you can get your opponent (B or R) with `self.opponent`.
+
+        # Find all the free tiles across the playable boards.
+        possible_moves = [
+            (board, tile)
+            for board in playable_boards
+            for tile in range(0, 9)
+            if boards[board][tile] is None
+        ]
+
+        # Pick a random valid move
+        return random.choice(possible_moves)
+
 
 def main():
-    # Store the owners of individual tiles
-    main_grid = [[None for _ in range(0,9)] for _ in range(0, 9)]
+    # Instantiate the agent
+    agent = Agent()
 
-    current_player = get_player()
-
-    while True:
-        # Get the next line of input and then split it at each space
-        parts = input().split(" ")
-        # Request Action
-        if parts[0] == "R":
-            playable_grids = [int(grid_pos) for grid_pos in parts[1].split(",")]
-
-            # For each playable grid find the tiles which are currently free
-            possible_moves = [{ 'grid': grid_pos, 'tile': tile_pos } for grid_pos in playable_grids for tile_pos in range(0, 9) if main_grid[grid_pos][tile_pos] is None]
-
-            move = random.choice(possible_moves)
-
-            print("M {} {}".format(move['grid'], move['tile']))
-        # Tile placed
-        elif parts[0] == "P":
-            player = parts[1]
-            grid_pos = int(parts[2])
-            tile_pos = int(parts[3])
-            main_grid[grid_pos][tile_pos] = player
-        # Grid won
-        elif parts[0] == "G":
-            # Either R, B or S (for stalemate) depending on who won the grid
-            winner = parts[1]
-            # The grid position
-            grid_pos = int(parts[2])
-            # This agent does not care about the overall grids as it only cares about playable valid moves
-            # in a real agent you will likely care about this though
+    # Start playing the game
+    game = UTTTGame(agent)
+    game.play()
 
 
-main()
+# This is a common Python idiom, which signals
+# to other Python programmers that this is a script.
+if __name__ == "__main__":
+    main()
