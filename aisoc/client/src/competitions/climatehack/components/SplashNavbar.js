3e83578c032aee9d1913aec21de0ec3d67d41038
Jeremy <jeremyloyingping@googlemail.com>
2022-01-01 21:50:15 +0000

    Make the Climate Hack challenge page look better on mobile.
    I've added a few tweaks to make things more responsive.

--- aisoc/client/src/competitions/climatehack/components/SplashNavbar.js
+++ aisoc/client/src/competitions/climatehack/components/SplashNavbar.js
@@ -3,8 +3,8 @@ import './SplashNavbar.scss';
 export default function SplashNavbar({ baseUrl }) {
   return <nav className="ch-navbar">
     <a href={baseUrl} className='ch-navbar-title'>Climate<span>Hack</span></a>
-    <a href={baseUrl}>Home</a>
-    <a href="#">The Challenge</a>
+    {/* <a href={baseUrl} className='ch-navbar-home'>Home</a> */}
+    <a href={`${baseUrl}challenge`}>The Challenge</a>
     <a href="#">Our Partners</a>
     <a href={`${baseUrl}compete`} className='ch-navbar-active'>Compete on Doxa
     </a>
