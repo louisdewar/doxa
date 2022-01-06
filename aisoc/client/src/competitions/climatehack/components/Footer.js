55f84d24d4c98b5d25ed17b413cb2bc747f8ec6f
Jeremy <jeremyloyingping@googlemail.com>
2022-01-06 23:33:56 +0000

    Update the Climate Hack challenge page styling.

--- aisoc/client/src/competitions/climatehack/components/Footer.js
+++ aisoc/client/src/competitions/climatehack/components/Footer.js
@@ -3,7 +3,7 @@ import './Footer.scss';
 export default function Footer() {
   return <footer className='ch-footer'>
     <div className='ch-footer-contact-us'>
-      <a href="mailto:jamie.weigold.19@ucl.ac.uk">Contact Us</a>
+      <a href="mailto:jamie.weigold.19@ucl.ac.uk">Contact us</a>
     </div>
     <div className='ch-footer-copyright'>
       Copyright &copy; {new Date().getFullYear()} <a href="https://uclaisociety.co.uk/">UCL AI Society</a>
