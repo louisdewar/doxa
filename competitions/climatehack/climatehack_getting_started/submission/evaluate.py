5632dd2dfed9afc479848c3743aa4d9403da5465
Jeremy <jeremyloyingping@googlemail.com>
2022-01-24 23:29:58 +0000

    Update the Climate Hack evaluation script.

--- competitions/climatehack/climatehack_getting_started/submission/evaluate.py
+++ competitions/climatehack/climatehack_getting_started/submission/evaluate.py
@@ -13,41 +13,44 @@ def main():
     model.load_state_dict(torch.load("model.pt"))
     model.eval()
 
+    print("STARTUP")
+
     try:
-        # get the input and output directory paths from doxa
+        # get the input and output directory paths from DOXA
         input_path = Path(sys.argv[1])
         output_path = Path(sys.argv[2])
-
-        # get the number of groups
-        group_count = int(sys.argv[3])
     except IndexError:
         raise Exception(
-            f"Run using: {sys.argv[0]} [input directory] [output directory] [group count]"
+            f"Run using: {sys.argv[0]} [input directory] [output directory]"
         )
 
     # process input group files
-    for i in range(group_count):
-        data = np.load(input_path / f"{i}.npz")["data"]
+    while True:
+        msg = input()
+        if not msg.startswith("Process "):
+            raise ValueError(f"Unknown messsage {msg}")
+
+        checkpoint_path = msg[8:]
+        data = np.load(input_path / checkpoint_path)["data"]
 
         # predict future satellite imagery for each array of 12 images
-        predictions = []
         with torch.no_grad():
+            predictions = []
             try:
                 for j in range(data.shape[0]):
                     prediction = model(
                         torch.from_numpy(data[j]).view(-1, 12 * 128 * 128)
                     )
                     predictions.append(prediction.view(12, 64, 64).detach().numpy())
-            except:
-                if not predictions:
-                    return
+            except Exception as err:
+                raise Exception(f"Error while processing {checkpoint_path}: {str(err)}")
 
             # save the group output
             np.savez(
-                output_path / f"{i}.npz",
+                output_path / checkpoint_path,
                 data=np.stack(predictions),
             )
-            print(f"Exported {i}")
+            print(f"Exported {checkpoint_path}")
 
 
 if __name__ == "__main__":
