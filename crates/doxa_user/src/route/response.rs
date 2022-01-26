20aaa58e61daca4c6128320836c57f254b148d11
Louis de Wardt <contact@louis.dewardt.uk>
2022-01-26 14:19:56 +0000

    get user info endpoint

--- crates/doxa_user/src/route/response.rs
+++ crates/doxa_user/src/route/response.rs
@@ -1,8 +1,10 @@
+use doxa_db::serde_json;
 use serde::Serialize;
 
 #[derive(Serialize)]
-pub struct UserInfo {
+pub struct PublicUserInfo {
     pub username: String,
     pub admin: bool,
     pub competitions: Vec<String>,
+    pub extra: serde_json::Value,
 }
