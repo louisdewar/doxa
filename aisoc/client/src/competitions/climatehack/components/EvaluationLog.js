165f97aea096aef56b8ef7c94a5603a5b65c75c9
Jeremy <jeremyloyingping@googlemail.com>
2022-01-27 11:40:53 +0000

    Show model images in the UI.

--- aisoc/client/src/competitions/climatehack/components/EvaluationLog.js
+++ aisoc/client/src/competitions/climatehack/components/EvaluationLog.js
@@ -10,38 +10,56 @@ function EvaluationLogCard({ event, hasForfeited }) {
   if (event.type == '_START' || event.type == '_END') {
     return <div className='ch-evaluation-log-card ch-evaluation-log-endpoint'>
       <div className='ch-evaluation-log-card-body'>
-        <FontAwesomeIcon icon={event.type == '_START' ? faHourglassStart : faHourglassEnd} size='sm' fixedWidth /> {event.type == '_START' ? 'This submission was received' : 'Evaluation terminated'} {formatTime(new Date(event.timestamp))}.
+        <FontAwesomeIcon icon={event.type == '_START' ? faHourglassStart : faHourglassEnd} size='sm' fixedWidth />
+        <div className='ch-evaluation-log-card-text'>
+          {event.type == '_START' ? 'This submission was received' : 'Evaluation terminated'} {formatTime(new Date(event.timestamp))}.
+        </div>
       </div>
     </div>;
   } else if (event.type == 'final') {
     return <div className='ch-evaluation-log-card'>
       <div className='ch-evaluation-log-card-body'>
-        <FontAwesomeIcon icon={faClock} size='sm' fixedWidth /> The final score for this submission is {roundScore(event.payload.score)}.
+        <FontAwesomeIcon icon={faClock} size='sm' fixedWidth />
+        <div className='ch-evaluation-log-card-text'>
+          The final score for this submission is {roundScore(event.payload.score)}.
+        </div>
       </div>
     </div>;
   } else if (event.type == '_FORFEIT') {
     return <div className='ch-evaluation-log-card ch-evaluation-log-forfeit'>
       <div className='ch-evaluation-log-card-body'>
-        <FontAwesomeIcon icon={faExclamationTriangle} size='sm' fixedWidth /> An error occurred while running this submission.
+        <FontAwesomeIcon icon={faExclamationTriangle} size='sm' fixedWidth />
+        <div className='ch-evaluation-log-card-text'>
+          An error occurred while running this submission.
+        </div>
       </div>
     </div>;
   } else if (event.type == '_ERROR' && !hasForfeited) {
     return <div className='ch-evaluation-log-card ch-evaluation-log-forfeit'>
       <div className='ch-evaluation-log-card-body'>
-        <FontAwesomeIcon icon={faExclamationTriangle} size='sm' fixedWidth /> An internal server error occurred during the evaluation of this submission.
+        <FontAwesomeIcon icon={faExclamationTriangle} size='sm' fixedWidth />
+        <div className='ch-evaluation-log-card-text'>
+          An internal server error occurred during the evaluation of this submission.
+        </div>
       </div>
     </div>;
   } else if (event.type == '_CANCELLED') {
     return <div className='ch-evaluation-log-card ch-evaluation-log-cancelled'>
       <div className='ch-evaluation-log-card-body'>
-        <FontAwesomeIcon icon={faBan} size='sm' fixedWidth /> The evaluation of this submission was cancelled.
+        <FontAwesomeIcon icon={faBan} size='sm' fixedWidth />
+        <div className='ch-evaluation-log-card-text'>
+          The evaluation of this submission was cancelled.
+        </div>
       </div>
     </div>;
   } else if (event.type.startsWith('checkpoint')) {
     return <div className='ch-evaluation-log-card'>
       <div className='ch-evaluation-log-card-body'>
         <FontAwesomeIcon icon={faFlagCheckered} size='sm' fixedWidth />
-        <strong>Checkpoint #{event.payload.checkpoint + 1}</strong> was reached {formatTime(new Date(event.timestamp))} with a score of {roundScore(event.payload.score)}.
+        <div className='ch-evaluation-log-card-text'>
+          <strong>Checkpoint #{event.payload.checkpoint + 1}</strong> was reached {formatTime(new Date(event.timestamp))} with a score of {roundScore(event.payload.score)}.
+        </div>
+        {event.payload.images && <img src={`data:image/png;base64,${event.payload.images[0]}`} />}
       </div>
     </div>;
   }
