e1b3fc35501a9460124153e4f15465766f7d898a
Louis de Wardt <contact@louis.dewardt.uk>
2022-01-27 10:32:40 +0000

    add matplotlib to scorer venv

--- aisoc/deploy/make_scorer_virtualenv.sh
+++ aisoc/deploy/make_scorer_virtualenv.sh
@@ -6,6 +6,6 @@ source "/scorer_env/bin/activate"
 
 python -m pip install --upgrade pip
 
-python -m pip install numpy pytorch-msssim
+python -m pip install numpy pytorch-msssim matplotlib
 python -m pip install torch==1.10.1+cpu torchvision==0.11.2+cpu -f https://download.pytorch.org/whl/cpu/torch_stable.html
 
