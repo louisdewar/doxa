90e4a5ad306146aac71df3bc24533d9db1025c1d
Louis de Wardt <contact@louis.dewardt.uk>
2022-01-29 00:19:05 +0000

    add universities trie

--- aisoc/client/src/utils/profile.js
+++ aisoc/client/src/utils/profile.js
@@ -1,6 +1,6 @@
 // TODO: maybe move this to a `models` folder and include other models for representing responses from API
 
-const UNIVERSITIES = {
+const UNIVERSITIES_DOMAIN_MAP = {
   'UNKNOWN': {
     name: 'Unknown',
     logo: null
@@ -8,14 +8,86 @@ const UNIVERSITIES = {
   'ucl.ac.uk': {
     name: 'UCL',
     logo: null
-  }
+  },
+  'warwick.ac.uk': {
+    name: 'Warwick',
+    logo: null
+  },
+  'gla.ac.uk': {
+    name: 'Glasgow',
+    logo: null
+  },
+  'princeton.edu': {
+    name: 'Princeton',
+    logo: null
+  },
+  'imperial.ac.uk': {
+    name: 'Princeton',
+    logo: null
+  },
 };
+
+function buildUniversitiesTrie(unis) {
+  let rootTrie = { trie: true };
+
+  for (const uniDomain in unis) {
+    const parts = uniDomain.split('.');
+    let trie = rootTrie;
+
+    for (let i = parts.length - 1; i >= 0; i--) {
+      const domainPart = parts[i];
+
+      if (!Object.prototype.hasOwnProperty.call(trie, domainPart)) {
+        trie[domainPart] = {
+          trie: true,
+        };
+      } else if (i == 0) {
+        throw new Error('Overlapping uni domains');
+      }
+
+      trie = trie[domainPart];
+    }
+
+    trie.trie = false;
+    trie.name = unis[uniDomain].name;
+    trie.logo = unis[uniDomain].logo;
+  }
+
+  return rootTrie;
+}
+
+function findUniversity(rootTrie, uniDomain) {
+  const parts = uniDomain.split('.');
+
+  let trie = rootTrie;
+  for (let i = parts.length - 1; i >= 0; i--) {
+    const part = parts[i];
+
+    if (!Object.prototype.hasOwnProperty.call(trie, part)) {
+      return UNIVERSITIES_DOMAIN_MAP['UNKNOWN'];
+    }
+
+    trie = trie[part];
+    // We've reached the leaf, this domain might be more specific but we stop at the first match
+    if (!trie.trie) {
+      return trie;
+    }
+
+
+  }
+
+  console.error('Reached end of domain without finding specific university, this domain is too general', uniDomain);
+  return UNIVERSITIES_DOMAIN_MAP['UNKNOWN'];
+}
+
+const UNIVERSITIES_TRIE = buildUniversitiesTrie(UNIVERSITIES_DOMAIN_MAP);
 
 export default class UserProfile {
   constructor(profile) {
     this.profile = profile;
 
-    this._university = UNIVERSITIES[(profile.extra && profile.extra.org) || 'UNKNOWN'] || UNIVERSITIES['UNKNOWN'];
+    this._university = findUniversity(UNIVERSITIES_TRIE, (profile.extra && profile.extra.org) || 'UNKNOWN');
+
   }
 
   name() {
