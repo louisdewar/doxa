c1e3d07e4a0e43ed97d3be7678aaa8ce93c03c84
Jeremy <jeremyloyingping@googlemail.com>
2022-01-31 21:18:24 +0000

    Highlight the logged in user's submission on the leaderboard.

--- aisoc/client/src/competitions/climatehack/components/Leaderboard.js
+++ aisoc/client/src/competitions/climatehack/components/Leaderboard.js
@@ -1,13 +1,14 @@
 import TextBox from 'components/TextBox';
+import { useAuth } from 'hooks/useAuth';
 import { useState } from 'react';
 import { Link } from 'react-router-dom';
 import { roundScore } from '../utils';
 import './Leaderboard.scss';
 
-function ClimateHackLeaderboardRow({ rank, score, user, baseUrl }) {
+function ClimateHackLeaderboardRow({ rank, score, user, baseUrl, highlightUser }) {
   return <div className='ch-leaderboard-entry'>
     <span className="ch-leaderboard-position">{rank}</span>
-    <span className="ch-leaderboard-username"><Link to={`${baseUrl}user/${user.name()}`}>{user.name()}</Link></span>
+    <span className={`ch-leaderboard-username ${highlightUser ? 'ch-leaderboard-username-highlighted' : ''}`}><Link to={`${baseUrl}user/${user.name()}`}>{user.name()}</Link></span>
     <span className="ch-leaderboard-university">{user.university().name}</span>
     <span className="ch-leaderboard-score">{String(score ? roundScore(score / 10000000) : 0.0).padEnd(7, '0')}</span>
   </div>;
@@ -15,6 +16,7 @@ function ClimateHackLeaderboardRow({ rank, score, user, baseUrl }) {
 
 
 export default function Leaderboard({ baseUrl, leaderboard }) {
+  const auth = useAuth();
   const [filter, setFilter] = useState('');
 
   const lowerFilter = filter.toLowerCase();
@@ -34,6 +36,13 @@ export default function Leaderboard({ baseUrl, leaderboard }) {
       <span className="ch-leaderboard-score">Score</span>
     </div>
 
-    {leaderboard.map((entry, i) => (entry.user.name().toLowerCase().includes(lowerFilter) || entry.user.university().name.toLowerCase().includes(lowerFilter)) && <ClimateHackLeaderboardRow key={i} rank={i + 1} score={entry.score} user={entry.user} baseUrl={baseUrl} />)}
+    {leaderboard.map((entry, i) => (entry.user.name().toLowerCase().includes(lowerFilter) || entry.user.university().name.toLowerCase().includes(lowerFilter)) && <ClimateHackLeaderboardRow
+      key={i}
+      rank={i + 1}
+      score={entry.score}
+      user={entry.user}
+      baseUrl={baseUrl}
+      highlightUser={auth.user && auth.user.username && auth.user.username == entry.user}
+    />)}
   </div>;
 }
