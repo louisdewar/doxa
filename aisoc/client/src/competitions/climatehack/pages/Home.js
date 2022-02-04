0862fb56b30591661a1e9aa3244cf0f60bfcbe30
Jeremy <jeremyloyingping@googlemail.com>
2022-02-04 16:37:06 +0000

    Add links to the discord to the Climate Hack website.
    I've added the link straight at the top lest people miss it.

--- aisoc/client/src/competitions/climatehack/pages/Home.js
+++ aisoc/client/src/competitions/climatehack/pages/Home.js
@@ -45,6 +45,8 @@ export default function Home({ baseUrl }) {
         {'Climate Hack.AI is an alliance between the artificial intelligence societies of some of the world\'s best universities in the fight against climate change. Your challenge is to beat the current best nowcasting techniques for UK satellite imagery.'}
         <br /><br />
         {'By helping to improve solar photovoltaic power output predictions, your model could help the National Grid Electricity System Operator minimise the use of standby natural gas turbines, potentially saving up to 100 kilotonnes of carbon emissions a year.'}
+        <br /><br />
+        Make sure to join our <a href="https://discord.gg/HTTQ8AFjJp">Discord</a> and follow us on <a href="https://linktr.ee/climatehack.ai">social media</a> to receive updates throughout the competition.
       </>}
     />
     <CompetitionTabs tabs={tabs} />
